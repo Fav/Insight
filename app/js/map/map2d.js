7ec9f315626ae414c7d7ca4808c01d7da885cd87
fav <f25457@qq.com>
2017-03-02 18:18:36 +0800

    二三维地图初始化

--- app/js/map/map2d.js
+++ app/js/map/map2d.js
@@ -6,14 +6,15 @@ require.config({
 });
 
 require(['jquery','leaflet'],function($,lf){
-	var map = L.map('map').setView([51.505, -0.09], 13);
+	var map = L.map('leafletContainer').setView([30.592747,114.305230], 13);
+
 
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-	L.marker([51.5, -0.09]).addTo(map)
-	    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-	    .openPopup();
+	// L.marker([51.5, -0.09]).addTo(map)
+	//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+	//     .openPopup();
 
 })
